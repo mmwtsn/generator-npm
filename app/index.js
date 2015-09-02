7c4e356095f60f367da4f9106d5f0884f380a047
M. Maxwell Watson <hi@mmwtsn.com>
2015-09-01 23:57:28 -0400

    Prompt user for input
    Add slugify module and collect these fields:
    - name         {string}
    - description  {string}
    - author       {string}
    - e-mail       {string}
    - open source? {boolean}
    - es6?         {boolean}

--- app/index.js
+++ app/index.js
@@ -1,13 +1,65 @@
 var chalk = require('chalk')
 var generators = require('yeoman-generator')
+var slugify = require('slugify')
 var yosay = require('yosay')
 
 module.exports = generators.Base.extend({
   prompting: function () {
+    var done = this.async()
+
     this.log(yosay(
       'Welcome to the interactive ' +
       chalk.red('ATTN:') +
       ' Node module generator!'
     ))
+
+    var prompts = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What\'s the name of your module?',
+        default: slugify(this.appname)
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What\'s this package do?',
+        default: 'Please fill me out'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Who is the author of this module?',
+        default: this.user.git.name
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is their e-mail address?',
+        default: this.user.git.email
+      },
+      {
+        type: 'confirm',
+        name: 'open',
+        message: 'Is this module open source?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'es6',
+        message: 'Finally, will it be written in ES6?',
+        default: true
+      }
+    ]
+
+    this.prompt(prompts, function (answers) {
+      this.answers = answers
+      this.answers.name = slugify(answers.name)
+
+      done()
+    }.bind(this))
+  },
+  log: function () {
+    console.log(this.answers)
   }
 })
