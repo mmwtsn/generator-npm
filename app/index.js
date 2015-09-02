b32158f3339a0cc43dcb8497ca8c992e9851846a
M. Maxwell Watson <hi@mmwtsn.com>
2015-09-01 23:49:13 -0400

    Adds yosay greeting

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
+var chalk = require('chalk')
 var generators = require('yeoman-generator')
+var yosay = require('yosay')
 
 module.exports = generators.Base.extend({
-  log: function () {
-    console.log('Hello from Yeoman!')
+  prompting: function () {
+    this.log(yosay(
+      'Welcome to the interactive ' +
+      chalk.red('ATTN:') +
+      ' Node module generator!'
+    ))
   }
 })
