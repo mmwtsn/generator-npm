b8393763085685e8cf93e544021949e123ce8377
M. Maxwell Watson <hi@mmwtsn.com>
2015-09-03 09:47:28 -0400

    Sets up ES6 test suite for generator
    Adds:
    - Babel dependency
    - Mocha dependency
    - Test directory
    - Mocha options file
    - Stubbed test

---
+++ test/index.js
@@ -0,0 +1,17 @@
+/* global beforeEach, describe, it */
+
+/**
+ * Module dependencies
+ *
+ * See the Yeoman documentation for the full list of helper functions
+ * {@link http://yeoman.github.io/generator/assert.html}
+ * {@link http://yeoman.github.io/generator/helpers.html}
+ */
+
+var assert = require('yeoman-generator').assert
+var helpers = require('yeoman-generator').test
+
+describe('generator-npm', () => {
+  describe('by default', () => {
+  })
+})
